047d80b830d100ae2f72b01edcce70dfeeddc4ab
btopro <bto108@psu.edu>
2012-08-17 14:39:30 -0400

    Ported to Drupal 7, that was fast!

--- js/book_title_override.js
+++ js/book_title_override.js
@@ -4,16 +4,16 @@ $(document).ready(function(){
 	$('#edit-book-title-override-override').click(function(){
 		// show if its checked for the override
 	  if ($(this).attr('checked') == true) {
-			$('#edit-book-title-override-title-wrapper').css('display', 'block');
+			$('.form-item-book-title-override-title').css('display', 'block');
 			$('#edit-book-title-override-title').focus();
 		}
 		else {
-			$('#edit-book-title-override-title-wrapper').css('display', 'none');
+			$('.form-item-book-title-override-title').css('display', 'none');
 		}
 	});
 	// account for default being checked already on page load
 	if ($('#edit-book-title-override-override').attr('checked') == true) {
-		$('#edit-book-title-override-title-wrapper').css('display', 'block');
+		$('.form-item-book-title-override-title').css('display', 'block');
 	}
 });
 })(jQuery);
